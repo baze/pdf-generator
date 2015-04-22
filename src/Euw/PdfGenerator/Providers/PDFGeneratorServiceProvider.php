45aaf990b37c9bb0daa0196a728a5864a6ecb29e
Björn Martensen <bjoern.martensen@gmail.com>
2015-04-22 11:42:55 +0200

    simplify service provider

--- src/Euw/PdfGenerator/Providers/PDFGeneratorServiceProvider.php
+++ src/Euw/PdfGenerator/Providers/PDFGeneratorServiceProvider.php
@@ -1,16 +1,39 @@
 <?php namespace Euw\PdfGenerator\Providers;
 
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
 class PDFGeneratorServiceProvider extends ServiceProvider {
 
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->bind('pdfgenerator', 'Euw\PdfGenerator\Generators\PDFGenerator');
-    }
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		$this->app->bind( 'pdfgenerator', 'Euw\PdfGenerator\Generators\PDFGenerator' );
+
+		$this->app->register( 'Euw\PdfGenerator\Providers\PDFRendererServiceProvider' );
+	}
+
+	public function boot() {
+		AliasLoader::getInstance()->alias( 'PDFGenerator', 'Euw\PdfGenerator\Facades\PDFGenerator' );
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides() {
+		return array();
+	}
+
 }
